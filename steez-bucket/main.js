a0e5bb2412aaeac4471c6a5cbddee557199e9324
MIlan Robinson <78048078+MilanrDev@users.noreply.github.com>
2021-05-16 22:07:44 -0400

    Add files via upload

--- steez-bucket/main.js
+++ steez-bucket/main.js
@@ -5,7 +5,35 @@ document.querySelector('button').addEventListener('click', press)
 
 function press() {
 
-if (document.querySelector('input').innerHTML === 'hello')
+  const clothes = document.getElementById('style').value
 
-alert('hellloo')
+if (clothes === 'chill' || clothes === 'casual' || clothes === 'relaxed' ){
+
 }
+
+  // document.getElementById('img')
+
+}
+
+
+
+var textWrapper = document.querySelector('.anima');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.anima .letter',
+    translateX: [40,0],
+    translateZ: 0,
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 1200,
+    delay: (el, i) => 500 + 30 * i
+  }).add({
+    targets: '.anima .letter',
+    translateX: [0,-30],
+    opacity: [1,0],
+    easing: "easeInExpo",
+    duration: 1100,
+    delay: (el, i) => 100 + 30 * i
+  });
